890a7e7cfe0b167226c0917b67532f2fc0fae187
Valenss <valentinschiaffino2@gmail.com>
2022-07-11 11:20:00 -0300

    push

---
+++ main.js
@@ -0,0 +1,7 @@
+const hamburgerBtn = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar');
+
+hamburgerBtn.addEventListener('click', e => {
+    navbar.classList.toggle('navbar__active');
+
+})
